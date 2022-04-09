ccdc926d5592a61c890483759b4296d85d45b8a2
chidieb11 <chukwuchidieberejohn@gmail.com>
2022-04-09 19:07:44 +0100

    Initial commit

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://unisex-clothing-store.herokuapp.com/api";
+const BASE_URL = "http://localhost:5000/api";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjNiMTVmNTU2MjVhNjI2ZDBjMTc5MWIiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2NDg2NzM5OTksImV4cCI6MTY0ODg0Njc5OX0.aSDjT5gmbCtmFWRAPqg3mwTR9LznDLdnzQDgMx5ouDg";
 
